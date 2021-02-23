ee75e0dfcd4b66f09206062209c18009c436928e
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-02-23 14:27:00 +0300

    Update OtobüsProgramı.java

--- OtobüsProgramı.java
+++ OtobüsProgramı.java
@@ -2,24 +2,24 @@ package projects;
 
 import java.util.Scanner;
 
-public class OtobüsProgramý {
+public class OtobÃ¼sProgramÄ± {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please enter the date (HH:MM) --> ");
-		String currentDate = input.next();//þuanki saat 
-		String[] hourAndMinute = currentDate.split(":");// saati ortadan bölüyor saat ve dakika diye split bölme yapar 
+		String currentDate = input.next();//ÅŸuanki saat 
+		String[] hourAndMinute = currentDate.split(":");
 		String otobusSaatleri[] = { "12:00", "13:00", "14:00" };
 
-		int currentHour = Integer.parseInt(hourAndMinute[0]);//bölümüþ kýsmýn saatini stringden inte çeviriyor
-		int currentMinute = Integer.parseInt(hourAndMinute[1]);//bölümüþ kýsmýn dakikasýný stringden inte çeviriyor
+		int currentHour = Integer.parseInt(hourAndMinute[0]);
+		int currentMinute = Integer.parseInt(hourAndMinute[1]);
 		int i;
-		// for döngüsü ile kontrol ediliyor
+		
 		for (i = 0; i < otobusSaatleri.length; i++) {
-			String[] otoSaat = otobusSaatleri[i].split(":");//dizinin içindeki saatleri bölüyor
-			int firstHour = Integer.parseInt(otoSaat[0]);// diziin içindeki saati stringden inte çeviriyor
-			int firstMinute = Integer.parseInt(otoSaat[1]);// diziin içindeki dakikayý stringden inte çeviriyor
-			// mesela 14:00 da otosaat0 dediðnide 0 olan 14 tür 1 olan 00 kýsmýdýr 
+			String[] otoSaat = otobusSaatleri[i].split(":");
+			int firstHour = Integer.parseInt(otoSaat[0]);
+			int firstMinute = Integer.parseInt(otoSaat[1]);
+			
 			
 			if (firstHour > currentHour) {
 				if (firstMinute >= currentMinute) {
