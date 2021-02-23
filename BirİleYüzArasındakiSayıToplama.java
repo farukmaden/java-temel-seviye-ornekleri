be63d1b073ac4dd38f74b28081a0cb73e06a5df7
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-02-23 16:42:11 +0300

    Create BirİleYüzArasındakiSayıToplama.java

---
+++ BirİleYüzArasındakiSayıToplama.java
@@ -0,0 +1,15 @@
+package projects;
+
+public class BirİleYüzArasındakiSayıToplama {
+
+	public static void main(String[] args) {
+		int toplam =0;
+		int sayı;
+		for(int i =1;i<100;i++) {
+			toplam=toplam+i;
+		}
+		System.out.println("toplam : "+ toplam);
+
+	}
+
+}
